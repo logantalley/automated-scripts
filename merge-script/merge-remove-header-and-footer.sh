a521a8ef07329bc57c076f46750029b26547a865
logantalley <ltalley@benefitsscience.com>
2019-10-02 13:21:39 -0400

    fixed typo

--- merge-script/merge-remove-header-and-footer.sh
+++ merge-script/merge-remove-header-and-footer.sh
@@ -1,7 +1,7 @@
 list=(*.txt)
 
 head -1 "${list[0]}" > tmp.txt
-sed '1d;$d' "${list[0]}" >> tmp.txt
+sed '1d;$d' "${list[@]}" >> tmp.txt
 echo "FOOTER" >> tmp.txt
 perl -pe 's/(?<!\r)\n/\r\n/g' < tmp.txt > combined.txt
 rm tmp.txt
