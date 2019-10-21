bd6a4e266f4f314ec0c3ad639e2744c5b7952d9a
logantalley <ltalley@benefitsscience.com>
2019-10-21 12:42:52 -0400

    Fixed the merge script

---
+++ merge-script/fixed_files_with_header_footer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+list=(*.txt)
+
+head -1 "${list[0]}" > tmp.txt
+for file in ${list[*]}
+do
+sed '1d;$d' $file >> tmp.txt
+done
+
+echo "FOOTER" >> tmp.txt
+perl -pe 's/(?<!\r)\n/\r\n/g' < tmp.txt > combined.txt
+rm tmp.txt
