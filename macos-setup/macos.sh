baac5250ccf7c80c68aa065a704141f0a837055e
logantalley <ltalley@benefitsscience.com>
2020-02-25 16:13:21 -0500

    Initial macOS script

---
+++ macos-setup/macos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo 'Downloading awscli'
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+
+echo 'Symlinking aws'
+sudo ln -s /usr/local/bin/aws2 /usr/local/bin/aws
+
+echo 'We need to setup your AWS credentials'
+aws configure
+
+echo 'Downloading macOS bundle'
+aws s3 cp s3://com.bstis.it.resources/macOS/macos.tar.gz macos.tar.gz
